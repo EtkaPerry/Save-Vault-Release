7c86db0a260c038b5b6289f83f4f008635443fa3
Etka <etkaperry@gmail.com>
2025-05-13 03:14:16 +0000

    To fit the save structure

--- Main/Utilities/KnownGames.cs
+++ Main/Utilities/KnownGames.cs
@@ -6,26 +6,25 @@ namespace SaveVaultApp.Utilities
     /// Maintains a database of known games with their folder names, executables, and save paths.
     /// </summary>
     public static class KnownGames
-    {
-        /// <summary>
+    {        /// <summary>
         /// List of known games with their folder names, executables, and save paths.
         /// </summary>
         public static readonly List<KnownGameInfo> GamesList = new List<KnownGameInfo>
         {
-            // Example format from user request
+            // Example format for easy adding for different type of game first have executable in path other is inside of the folder
             new KnownGameInfo
             {
                 Name = "Cyberpunk 2077",
                 GameFolder = "Cyberpunk 2077",
                 Executable = "REDprelauncher.exe",
-                SavePath = "%USERPROFILE%/Saved Games/CD Projekt Red/Cyberpunk 2077"
-            },
+                SavePath = "%USERPROFILE%\\Saved Games\\CD Projekt Red\\Cyberpunk 2077"
+            },            
             new KnownGameInfo
             {
                 Name = "Abiotic Factor",
                 GameFolder = "AbioticFactor",
-                Executable = "AbioticFactor.exe",
-                SavePath = "%USERPROFILE%/AppData/Local/AbioticFactor/Saved"
+                Executable = "AbioticFactor\\Binaries\\Win64\\AbioticFactor-Win64-Shipping.exe",
+                SavePath = "%USERPROFILE%\\AppData\\Local\\AbioticFactor\\Saved"
             }
             // Additional games can be added here
         };

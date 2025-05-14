4ef9304af14e416ebe8a14c0530e7e9d9579f122
Etka <etkaperry@gmail.com>
2025-05-14 19:39:05 +0000

    Add known games

--- Main/Utilities/KnownGames.cs
+++ Main/Utilities/KnownGames.cs
@@ -35,6 +35,27 @@ namespace SaveVaultApp.Utilities
                 LaunchFromSteam = "steam://run/427410",
                 Uninstall = "steam://uninstall/427410",
                 Store = "steam://store/427410"
+            },            
+            new KnownGameInfo
+            {
+                Name = "RimWorld",
+                GameFolder = "RimWorld",
+                Executable = "RimWorldWin64.exe",
+                SavePath = "%USERPROFILE%\\AppData\\LocalLow\\Ludeon Studios\\RimWorld by Ludeon Studios\\Saves",
+                Platform = "Steam",
+                LaunchFromSteam = "steam://run/294100",
+                Uninstall = "steam://uninstall/294100",
+                Store = "steam://store/294100"
+            },              new KnownGameInfo
+            {
+                Name = "Europe Universalis IV",
+                GameFolder = "Europa Universalis IV",
+                Executable = "eu4.exe",
+                SavePath = "%STEAMPATH%\\userdata\\*\\236850\\remote",
+                Platform = "Steam",
+                LaunchFromSteam = "steam://run/236850",
+                Uninstall = "steam://uninstall/236850",
+                Store = "steam://store/236850"
             }
             // Additional games can be added here
         };
